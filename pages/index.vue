69e94c30a80c86ec42634f508ddf60f5e7ca2ff6
Ernie Jeash Villahermosa <irenesejah29@gmail.com>
2023-05-16 17:51:07 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <section class="h-screen w-screen relative flex items-center justify-center">
+      <div class="bg-[url(/assets/svg/grid.svg)] absolute inset-0 bg-center"></div>
+      <div class="max-w-[70ch] text-center flex flex-col gap-5">
+        <h1 class="text-blue-500 text-2xl font-semibold">
+          We are symph
+        </h1>
+
+        <h2 class="text-[48px] font-medium leading-[1.4]">
+          We are a team of people passionate about building products that people love.
+        </h2>
+      </div>
+    </section>
+
+    <section>
+      <h1 class="text-blue-500 text-2xl font-semibold">
+        We are symph
+      </h1>
+    </section>
+  </div>
+</template>
+
